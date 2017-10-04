c9a4c8363881ff1a97081a2bc0acf0d57fa439c1
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-10-05 01:40:56 +0700

    initialize HomeFragment when create MainActivity

--- LnwShop/app/src/main/java/com/homework06/lnwshop/MainActivity.java
+++ LnwShop/app/src/main/java/com/homework06/lnwshop/MainActivity.java
@@ -3,13 +3,16 @@ package com.homework06.lnwshop;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.widget.FrameLayout;
+
+import com.homework06.lnwshop.fragment.HomeFragment;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    private FrameLayout fragmentContainer;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -18,13 +21,10 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_feature);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_forum);
                     return true;
             }
             return false;
@@ -37,9 +37,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+        initialFragment();
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
+    private void initialFragment() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .add(R.id.fragmentContainer, new HomeFragment())
+                .commit();
+    }
 }
