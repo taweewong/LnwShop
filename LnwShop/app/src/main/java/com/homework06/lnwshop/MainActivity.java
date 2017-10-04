24b2ad756c639a24ae22893d77ef77e03c40bd4d
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-10-05 00:24:12 +0700

    set text for Bottom Navigation Bar

--- LnwShop/app/src/main/java/com/homework06/lnwshop/MainActivity.java
+++ LnwShop/app/src/main/java/com/homework06/lnwshop/MainActivity.java
@@ -21,10 +21,10 @@ public class MainActivity extends AppCompatActivity {
                     mTextMessage.setText(R.string.title_home);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    mTextMessage.setText(R.string.title_feature);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    mTextMessage.setText(R.string.title_forum);
                     return true;
             }
             return false;
