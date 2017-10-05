6189351801367fab3f29ee16d16c280c27ca54f1
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-10-06 00:45:04 +0700

    created function openActivity in class CartActivity

--- LnwShop/app/src/main/java/com/homework06/lnwshop/fragment/CartActivity.java
+++ LnwShop/app/src/main/java/com/homework06/lnwshop/fragment/CartActivity.java
@@ -1,10 +1,12 @@
 package com.homework06.lnwshop.fragment;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.LinearLayout;
 
+import com.homework06.lnwshop.PaymentSuccessActivity;
 import com.homework06.lnwshop.R;
 
 public class CartActivity extends AppCompatActivity {
@@ -20,8 +22,13 @@ public class CartActivity extends AppCompatActivity {
         checkoutLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                openActivity(PaymentSuccessActivity.class);
             }
         });
+    }
+
+    private void openActivity(Class destinationClass) {
+        Intent intent = new Intent(this, destinationClass);
+        startActivity(intent);
     }
 }
