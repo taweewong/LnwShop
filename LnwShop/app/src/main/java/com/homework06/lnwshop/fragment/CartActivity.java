c832429e5ca486aa635503d17204a5972cee3078
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-10-06 00:15:52 +0700

    setOnClickListener to checkoutLayout in class CartActivity

--- LnwShop/app/src/main/java/com/homework06/lnwshop/fragment/CartActivity.java
+++ LnwShop/app/src/main/java/com/homework06/lnwshop/fragment/CartActivity.java
@@ -2,14 +2,26 @@ package com.homework06.lnwshop.fragment;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import com.homework06.lnwshop.R;
 
 public class CartActivity extends AppCompatActivity {
 
+    LinearLayout checkoutLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
+
+        checkoutLayout = (LinearLayout) findViewById(R.id.checkoutLayout);
+        checkoutLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
