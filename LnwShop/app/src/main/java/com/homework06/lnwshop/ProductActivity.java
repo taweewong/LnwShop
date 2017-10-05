bc4e021b74fe521cb26b1ffcb554e192f9e59b4f
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-10-06 01:54:45 +0700

    inflated menu to ActionBar and show dialog when click add item in class ProductActivity

--- LnwShop/app/src/main/java/com/homework06/lnwshop/ProductActivity.java
+++ LnwShop/app/src/main/java/com/homework06/lnwshop/ProductActivity.java
@@ -1,15 +1,22 @@
 package com.homework06.lnwshop;
 
+import android.app.FragmentManager;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
 
+import com.homework06.lnwshop.fragment.ProductDialogFragment;
+
 public class ProductActivity extends AppCompatActivity {
 
     LinearLayout addItemLayout;
+    private Menu menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +27,10 @@ public class ProductActivity extends AppCompatActivity {
         addItemLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openActivity(CartActivity.class);
+                menu.getItem(0).setIcon(ContextCompat.getDrawable(ProductActivity.this, R.drawable.ic_cart_item_menu));
+                FragmentManager fragmentManager = getFragmentManager();
+                ProductDialogFragment dialogFragment = new ProductDialogFragment();
+                dialogFragment.show(fragmentManager, "ProductDialog");
             }
         });
 
@@ -30,10 +40,21 @@ public class ProductActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        this.menu = menu;
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.cart_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
             finish();
             return true;
+        } else if (item.getItemId() == R.id.cartMenu) {
+            openActivity(CartActivity.class);
         }
         return super.onOptionsItemSelected(item);
     }
