43fec74287f132bba180a82752d127333582204a
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-10-05 23:52:07 +0700

    setOnClickListener to addItemLayout in class ProductActivity

--- LnwShop/app/src/main/java/com/homework06/lnwshop/ProductActivity.java
+++ LnwShop/app/src/main/java/com/homework06/lnwshop/ProductActivity.java
@@ -2,12 +2,24 @@ package com.homework06.lnwshop;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class ProductActivity extends AppCompatActivity {
+
+    LinearLayout addItemLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product);
+
+        addItemLayout = (LinearLayout) findViewById(R.id.addItemLayout);
+        addItemLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
