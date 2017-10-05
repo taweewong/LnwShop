fbf6f34162a7cbbe7288e6dd540b522cc7730110
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-10-06 00:08:00 +0700

    created CartActivity

---
+++ LnwShop/app/src/main/java/com/homework06/lnwshop/fragment/CartActivity.java
@@ -0,0 +1,15 @@
+package com.homework06.lnwshop.fragment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.homework06.lnwshop.R;
+
+public class CartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+    }
+}
