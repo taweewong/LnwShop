31217832c01b29394b4cd5b49866ab129dad5894
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-10-06 01:04:43 +0700

    set ActionBar HomeButton and SearchView in class SearchResultActivity

--- LnwShop/app/src/main/java/com/homework06/lnwshop/SearchResultActivity.java
+++ LnwShop/app/src/main/java/com/homework06/lnwshop/SearchResultActivity.java
@@ -3,6 +3,10 @@ package com.homework06.lnwshop;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
 
@@ -22,6 +26,41 @@ public class SearchResultActivity extends AppCompatActivity {
                 openActivity(ProductActivity.class);
             }
         });
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.search_menu, menu);
+        MenuItem item = menu.findItem(R.id.searchMenu);
+        SearchView searchView = (SearchView) item.getActionView();
+        searchView.setMaxWidth(Integer.MAX_VALUE);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void openActivity(Class destinationClass) {
