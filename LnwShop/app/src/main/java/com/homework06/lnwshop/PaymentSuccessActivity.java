c6f6cd4f41ef469cf7dff33ca20b909c9950ca6f
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-10-06 00:45:33 +0700

    created function openActivity in class PaymentSuccessActivity

--- LnwShop/app/src/main/java/com/homework06/lnwshop/PaymentSuccessActivity.java
+++ LnwShop/app/src/main/java/com/homework06/lnwshop/PaymentSuccessActivity.java
@@ -1,13 +1,32 @@
 package com.homework06.lnwshop;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class PaymentSuccessActivity extends AppCompatActivity {
+
+    Button backToMainButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment_success);
+
+        backToMainButton = (Button) findViewById(R.id.backToMainButton);
+        backToMainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity(MainActivity.class);
+                finish();
+            }
+        });
+    }
+
+    private void openActivity(Class destinationClass) {
+        Intent intent = new Intent(this, destinationClass);
+        startActivity(intent);
     }
 }
