cf2a0b25b174cd3835a578b382a0d3da2f1d0b19
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-10-06 00:45:54 +0700

    created function openActivity in class SearchResultActivity

--- LnwShop/app/src/main/java/com/homework06/lnwshop/SearchResultActivity.java
+++ LnwShop/app/src/main/java/com/homework06/lnwshop/SearchResultActivity.java
@@ -1,13 +1,31 @@
 package com.homework06.lnwshop;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class SearchResultActivity extends AppCompatActivity {
+
+    LinearLayout itemLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_result);
+
+        itemLayout = (LinearLayout) findViewById(R.id.itemLayout);
+        itemLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity(ProductActivity.class);
+            }
+        });
+    }
+
+    private void openActivity(Class destinationClass) {
+        Intent intent = new Intent(this, destinationClass);
+        startActivity(intent);
     }
 }
